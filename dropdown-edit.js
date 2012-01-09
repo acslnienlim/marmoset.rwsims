16f31b06f5ba059ed84e8a46dff0aba008e1a7c0
Ryan W Sims <rwsims@gmail.com>
2012-01-09 17:02:20 -0500

    Implemented rudiments of dropdown widget.

--- dropdown-edit.js
+++ dropdown-edit.js
@@ -1,7 +1,30 @@
 /** Wraps a <select /> tag for displaying a dropdown rubric. */
 function DropdownWidget(select) {
-    this.select = select;
+    this.$select = $(select)
+    this.valueMap = {};
 }
+
+DropdownWidget.prototype.redraw = function() {
+    this.$select.empty();
+    var frag = document.createDocumentFragment();
+    for (k in this.valueMap) {
+        option = document.createElement('option');
+        option.innerHTML = k + " [" + this.valueMap[k] + "]";
+        frag.appendChild(option);
+    }
+    this.$select.append(frag);
+}
+
+DropdownWidget.prototype.put = function(name, score) {
+    this.valueMap[name] = score;
+    this.redraw();
+}
+
+DropdownWidget.prototype.remove = function(name) {
+    delete this.valueMap[name];
+}
+
+$("#edit-dialog").dialog({autoOpen: false});
 
 
 /** Manages the dynamic list of rubrics in a table of rubrics. */
@@ -28,18 +51,16 @@ RubricManager.prototype.addDropdown = function() {
     var selectId = "#dropdown-select-" + this.rubricCount;
     var widget = new DropdownWidget($(selectId));
     $(buttonId).click(function(event) {
-        alert(selectId);
         event.preventDefault();
+        widget.put("foo", "bar");
     });
 }
 
 
 window.$marmoset = {
-    editDialog: $("#edit-dialog"),
+    editDialog: $("#edit-dialog").dialog({autoOpen: false}),
     manager: new RubricManager($("#rubric-table")),
 };
-
-$("#edit-dialog").dialog({autoOpen: false});
 
 $("#add-dropdown-button").click(function(event) {
     event.preventDefault();
